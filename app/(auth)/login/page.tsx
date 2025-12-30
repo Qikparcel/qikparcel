34db289bff6b2f36ec1f5c526c8911e127ce66d8
noman ashraf <naumanashraf30@gmail.com>
2025-12-30 22:30:43 +0200

    removed the typo

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -255,7 +255,7 @@ export default function LoginPage() {
               className="font-medium"
               style={{ color: "#29772F" }}
             >
-              Sign up HEHEHEHEHEH
+              Sign up
             </Link>
           </p>
         </div>
